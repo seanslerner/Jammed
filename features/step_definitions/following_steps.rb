a41e26a0e5fd39f2f0f60c125c2b9d4cf8a79585
Sean Lerner <seanslerner@gmail.com>
2012-04-14 09:59:33 -0700

    retrieve_following.feature WebMocked

--- features/step_definitions/following_steps.rb
+++ features/step_definitions/following_steps.rb
@@ -1,24 +1,35 @@
 When /^I get the users they follow$/ do
-  followings = Jammed::Following.new(@username)
-  serve_response("http://api.thisismyjam.com/1/IFTFOM/followers.json?key=987bcab01b929eb2c07877b224215c92", :following)
+  followings = Jammed::Following.new('IFTFOM')
+  serve_response("http://api.thisismyjam.com/1/IFTFOM/following.json?key=987bcab01b929eb2c07877b224215c92", :following)
   @followings = followings.following
 end
 
 Then /^I should see the users they follow$/ do
-  @followings.to_s.should include("TWHarr")
+  @followings.should_not be_nil
 end
 
 When /^I get the users they follow by "([^"]*)"$/ do |order|
-  @followings = Jammed::Following.new(@username).following(:order => "#{order}".to_sym)
+  followings = Jammed::Following.new('IFTFOM')
+  case(order)
+  when 'date'
+    serve_response("http://api.thisismyjam.com/1/IFTFOM/following.json?key=987bcab01b929eb2c07877b224215c92&order=when", :following_when)
+    @followings = followings.following(:order => :date)
+  when 'affinity'
+    serve_response("http://api.thisismyjam.com/1/IFTFOM/following.json?key=987bcab01b929eb2c07877b224215c92&order=affinity", :following_affinity)
+    @followings = followings.following(:order => :affinity)
+  when 'alpha'
+    serve_response("http://api.thisismyjam.com/1/IFTFOM/following.json?key=987bcab01b929eb2c07877b224215c92&order=alpha", :following_alpha)
+    @followings = followings.following(:order => :alpha)
+  end
 end
 
 Then /^I should see the users they follow ordered by "([^"]*)"$/ do |order|
   case(order)
   when "date"
-    @followings[-1].to_s.should include('TWHarr')
-  when "likes"
+    @followings[-1]["name"].should == "kateconlow"
+  when "affinity"
     @followings.should_not be_nil
   when "alpha"
-    @followings[-1].to_s.should include('TWHarr')
+    @followings[-1]["name"].should == "AlexEvans"
   end
 end
