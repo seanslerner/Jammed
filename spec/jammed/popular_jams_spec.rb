3bf4c67268f7ba437fddfe461e83cdd7af954aba
Rob Young <robyoung26@gmail.com>
2012-04-14 12:40:48 +0100

    Mock API Responses.

--- spec/jammed/popular_jams_spec.rb
+++ spec/jammed/popular_jams_spec.rb
@@ -4,7 +4,12 @@ module Jammed
   describe PopularJams do 
 
     let(:pop_jams) { Jammed::PopularJams.new }
-    describe "#popular_jams" do 
+    describe "#popular_jams" do
+      
+      before do
+        serve_response("http://api.thisismyjam.com/1/popular.json?key=987bcab01b929eb2c07877b224215c92", :popular)
+      end
+      
       it "parses the JSON return into a hash" do
         pop_jams.popular_jams.should be_an_instance_of Hash
       end
