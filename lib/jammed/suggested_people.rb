f62d8f33bb4f7f2db219c4df5832deaea29f6f81
Rob Young <robyoung26@gmail.com>
2012-04-15 14:04:36 +0100

    Improve suggested_people.
    This should be enough to close #4.

--- lib/jammed/suggested_people.rb
+++ lib/jammed/suggested_people.rb
@@ -1,14 +1,14 @@
 module Jammed
   class SuggestedPeople
-    attr_accessor :people
-
-    include HTTParty
-
-    base_uri 'http://api.thisismyjam.com/1/'
-
-    def people
-      @people ||= self.class.get "/suggestedPeople.json?key=#{API_KEY}"
+    
+    class Search
+      include HTTParty
+      base_uri 'http://api.thisismyjam.com/1'
     end
-
+    
+    def self.people
+      response = Search.get "/suggestedPeople.json?key=#{API_KEY}"
+      response["people"][0] ? response["people"] : "No people found"
+    end
   end
 end
--- spec/jammed/suggested_people_spec.rb
+++ spec/jammed/suggested_people_spec.rb
@@ -1,28 +1,14 @@
 require 'spec_helper'
 
 module Jammed
-  describe SuggestedPeople do 
-
-    let(:suggested_people) { Jammed::SuggestedPeople.new() }
-
-    describe "#initialize" do
-      it "initialises successfully" do
-        suggested_people.should_not be_nil
-      end
-    end
-    
-    describe "#people" do
-      
+  describe SuggestedPeople do
+    describe ".people" do
       before do
         serve_response("http://api.thisismyjam.com/1/suggestedPeople.json?key=987bcab01b929eb2c07877b224215c92", :suggested_people)
       end
       
-      it "parses the JSON return into a hash" do
-        suggested_people.people.should be_an_instance_of Hash
-      end
-      
-      it "gets the data from the api" do
-        suggested_people.people["people"].should_not be_nil
+      it "returns people from the API" do
+        Jammed::SuggestedPeople.people.should_not be_nil
       end
     end
   end
