8f9e7523bd8dbc16873e3872c9938760817abcc3
Sean Lerner <seanslerner@gmail.com>
2012-04-16 14:43:27 -0700

    added dynamic attributes as class methods to Jammed::Person

--- lib/jammed/person.rb
+++ lib/jammed/person.rb
@@ -10,5 +10,9 @@ module Jammed
       profile = Search.get "/#{opts[:username]}.json?key=#{API_KEY}"
       profile["person"] ? profile["person"] : "404: User Not Found"
     end
+
+    def method_missing(name, *args, &block)
+      self.profile(args).has_key?(name.to_s) ? self.profile(args)[name.to_s] : super
+    end
   end
 end
--- spec/jammed/person_spec.rb
+++ spec/jammed/person_spec.rb
@@ -17,5 +17,21 @@ module Jammed
         Jammed::Person.profile(:username => 'IFTFOM').should be_an_instance_of Hash
       end
     end
+
+    describe "dynamic attribute" do
+      
+      before do
+        serve_response("http://api.thisismyjam.com/1/IFTFOM.json?key=987bcab01b929eb2c07877b224215c92", :person)
+      end
+      
+      it "returns the attribute value if present in profile" do
+        pending "Jammed::Person has a method #name inherited from Module causing this to fail. Needs workaround."
+        Jammed::Person.name(:username => 'IFTFOM').should == "IFTFOM"
+      end
+
+      it "raises method missing if attribute is not present" do
+        lambda { Jammed::Person.foo_attribute }.should raise_error NoMethodError
+      end
+    end
   end
 end
